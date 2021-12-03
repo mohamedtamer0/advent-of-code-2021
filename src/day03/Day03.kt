8da7b7332ef08d0179e179565d3f05c2a175c61e
mohamedtamer0 <mt4452242@gmail.com>
2021-12-03 20:58:59 +0200

    Day 02

---
+++ src/day03/Day03.kt
@@ -0,0 +1,46 @@
+package day03
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val freq = StringBuilder()
+        val rare = StringBuilder()
+        for (i in input[0].indices) {
+            val zeros = input.count { it[i] == '0' }
+            val ones = input.size - zeros
+            freq.append(if (zeros > ones) '0' else '1')
+            rare.append(if (zeros <= ones) '0' else '1')
+        }
+        return freq.toString().toInt(2) * rare.toString().toInt(2)
+    }
+
+    fun part2(input: List<String>): Int {
+
+        fun findOnly(mostFrequent: Boolean): Int {
+            val copy = input.toMutableList()
+            for (i in input[0].indices) {
+                val zeros = copy.count { it[i] == '0' }
+                val ones = copy.size - zeros
+                val toKeep =
+                    if (zeros <= ones) if (mostFrequent) '1' else '0'
+                    else if (mostFrequent) '0' else '1'
+                copy.removeIf { it[i] != toKeep }
+                if (copy.size == 1) return copy.first().toInt(2)
+            }
+            error("Didn't find the only element")
+        }
+
+        return findOnly(true) * findOnly(false)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 198)
+
+    val input = readInput("Day03")
+    println(part1(input))
+
+    check(part2(testInput) == 230)
+    println(part2(input))
+}
